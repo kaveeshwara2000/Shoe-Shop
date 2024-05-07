7bb690150ee10c9b2a7e8c592eb96576154fea84
kaveeshwara2000 <sachindukaveeshwara@gmail.com>
2024-05-07 21:38:23 +0530

    Project created

---
+++ src/main/java/lk/ijse/shoe_shop/ShoeShopApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoe_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lk/ijse/shoe_shop/ShoeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.shoe_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
